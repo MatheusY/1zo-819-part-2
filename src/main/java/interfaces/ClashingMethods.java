4fab8299938892d28fe8c8ff9dda87205ba72ea9
matheus yudi <matheus.matsubara12@gmail.com>
2021-04-02 20:26:28 -0300

    Init Project

---
+++ src/main/java/interfaces/ClashingMethods.java
@@ -0,0 +1,31 @@
+package interfaces;
+
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 3: Interfaces
+Topic:  Method clashing
+*/
+
+interface FirstInterface {
+
+    // default method
+    default void methodA() {
+        System.out.println("FirstInterface's default method A");
+    }
+}
+
+interface SecondInterface {
+    // default method
+    default void methodA() {
+        System.out.println("SecondInterface's default method A");
+    }
+}
+
+public class ClashingMethods implements FirstInterface {
+	public static void main(String[] args) {
+		ClashingMethods first = new ClashingMethods();
+		// Calling static method on an interface
+		first.methodA();
+	}
+}
