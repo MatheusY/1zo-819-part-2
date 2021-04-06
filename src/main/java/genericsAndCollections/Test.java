d13007193999543199d93cda15172721e8252cbd
matheus yudi <matheus.matsubara12@gmail.com>
2021-04-06 07:19:17 -0300

    Collections

---
+++ src/main/java/genericsAndCollections/Test.java
@@ -0,0 +1,18 @@
+package genericsAndCollections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Long> list1 = new ArrayList<Long>(); // Line 1
+        list1.add(5L);   // Line 2
+        List<? extends Number> list2 = null;  // Line 3
+ 
+        if (list1 instanceof List<?>) { // Line 4
+            list2 = list1;      // Line 5
+        }
+ 
+        System.out.println(list2);
+    }
+}
