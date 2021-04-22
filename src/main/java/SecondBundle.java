3084bbc3d177a02ec1cefcb708e7cd911286138c
matheus yudi <matheus.matsubara12@gmail.com>
2021-04-22 07:20:50 -0300

    Localization

---
+++ src/main/java/SecondBundle.java
@@ -0,0 +1,24 @@
+
+
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 16:  Localization
+Topic:  ListResourceBundle
+*/
+
+import java.util.ListResourceBundle;
+
+import locale.Employee;
+
+public class SecondBundle extends ListResourceBundle {
+
+    protected Object[][] getContents() {
+        return new Object[][]{
+                {"default", "Yes"},
+                {"synonyms", new String[]{"Yea", "Yep", "Sure", "Of course"}},
+                {"jane", new Employee("Jane", "HR", "Manager")},
+                {"age", 40}
+        };
+    }
+}
